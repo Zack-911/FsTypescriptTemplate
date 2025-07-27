b288f976387171188f6c6a98030d7c9ed3af8199
Zack-911 <muhammadbinasim471@gmail.com>
2025-07-27 10:58:46 +0200

    Setup index file

---
+++ src/index.ts
@@ -0,0 +1,47 @@
+import { ForgeClient } from "@tryforge/forgescript"
+import { ForgeDB } from "@tryforge/forge.db"
+import { ForgeCanvas } from "@tryforge/forge.canvas"
+import * as dotenv from "dotenv"
+
+dotenv.config()
+
+const db: ForgeDB = new ForgeDB({
+    type: "better-sqlite3",
+    events: [
+        "connect",
+        "variableCreate",
+        "variableUpdate",
+        "variableDelete",
+    ],
+})
+
+const client: ForgeClient = new ForgeClient({
+    prefixes: [
+        ".",
+        "<@$botID>",
+        "<@!$botID>",
+    ],
+    intents: [
+        "Guilds",
+        "GuildMessages",
+        "GuildMembers",
+        "MessageContent",
+        "DirectMessages",
+        "DirectMessageReactions",
+        "DirectMessageTyping",
+    ],
+    events: [
+        "ready",
+        "messageCreate",
+        "interactionCreate",
+    ],
+    extensions: [
+        db,
+        new ForgeCanvas(),
+    ],
+})
+
+client.commands.load("dist/commands")
+client.applicationCommands.load("dist/slash")
+
+client.login(process.env.TOKEN)
